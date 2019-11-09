e5431542091a6aca0a6df9c23e1f7bb5ce2d507f
Melissa Kemp <otterspawdesign@gmail.com>
2019-11-08 19:21:40 -0700

    finished header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    // Element Creation
+    const div = document.createElement("div");
+    const dateSpan = document.createElement("span");
+    const h1 = document.createElement("h1");
+    const tempSpan = document.createElement("span");
+  
+    // Class Additions
+    div.classList.add("header");
+    dateSpan.classList.add("date");
+    tempSpan.classList.add("temp");
+  
+    //Adding Text Content
+    dateSpan.textContent = "SMARCH 28, 2019";
+    h1.textContent = "Lambda Times";
+    tempSpan.textContent = "98º";
+  
+    //Appending Elements
+    div.appendChild(dateSpan);
+    div.appendChild(h1);
+    div.appendChild(tempSpan);
+  
+    return div;
+  }
+  
+  const header = document
+    .querySelector(".header-container")
+    .appendChild(Header());
